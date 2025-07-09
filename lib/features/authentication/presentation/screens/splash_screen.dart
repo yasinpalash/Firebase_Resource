02fdd2d7be59fb3b36252ca5d11101ce828477b2
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-07-09 11:04:41 +0600

    refactor: Clean up imports in splash_screen.dart

--- lib/features/authentication/presentation/screens/splash_screen.dart
+++ lib/features/authentication/presentation/screens/splash_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'home_screen.dart';
 import 'singup.dart';
 
